dda453c3e5f2596ba14de2401e30202beb691fe4
Shamit Verma <github@shamit.in>
2019-03-04 13:08:18 +0530

    Code ofr training and saving the model

--- train_model.py
+++ train_model.py
@@ -1,11 +1,29 @@
-import keras
+import os
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers. normalization import BatchNormalization
-import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
 
-def define_model(image_dim,num_channels):
+
+def get_model_dir():
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    model_dir = os.path.join(script_dir,'data','model')
+    return os.path.normpath(model_dir)
+
+
+def get_image_dir():
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    image_dir = os.path.join(script_dir,'data','images')
+    return os.path.normpath(image_dir)
+
+
+def get_image_dir_for_class(image_class):
+    class_dir = os.path.join(get_image_dir(),image_class)
+    return os.path.normpath(class_dir)
+
+
+def define_model(image_dim,num_channels,num_classes):
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(image_dim, image_dim, num_channels)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -25,6 +43,54 @@ def define_model(image_dim,num_channels):
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dense(2, activation='softmax'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
     return model
 
+
+def get_data_generators(image_dim,batch_size):
+    image_dir=get_image_dir()
+    data_generator = ImageDataGenerator(validation_split=0.2)
+    train_generator = data_generator.flow_from_directory(image_dir, target_size=(image_dim, image_dim),
+                                                         shuffle=True, seed=13,
+                                                         class_mode='categorical', batch_size=batch_size,
+                                                         subset="training")
+
+    validation_generator = data_generator.flow_from_directory(image_dir, target_size=(image_dim, image_dim),
+                                                         shuffle=True, seed=13,
+                                                         class_mode='categorical', batch_size=batch_size,
+                                                              subset="validation")
+    return train_generator , validation_generator
+
+
+num_channels = 3 #RGB images
+
+image_dim = 100
+batch_size = 32
+
+train_generator , validation_generator = get_data_generators(image_dim,batch_size)
+
+num_train_images=train_generator.samples
+num_test_images=validation_generator.samples
+
+print('Got generators')
+
+
+
+model = define_model(image_dim,num_channels,train_generator.num_classes)
+
+model.summary()
+
+model.fit_generator(
+        train_generator,
+        epochs=5,
+        validation_data=validation_generator)
+
+model_dir = get_model_dir()
+os.makedirs(model_dir,exist_ok=True)
+model_file = os.path.join(model_dir,'trained_model.h5')
+model.save()
