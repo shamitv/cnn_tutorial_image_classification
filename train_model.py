48f49b65f24dfa1eb00fdf1e1035ebf53ef54ac2
Shamit Verma <github@shamit.in>
2019-03-04 15:35:29 +0530

    More layers in model

--- train_model.py
+++ train_model.py
@@ -31,7 +31,6 @@ def define_model(image_dim,num_channels,num_classes):
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
-    '''
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
@@ -41,7 +40,6 @@ def define_model(image_dim,num_channels,num_classes):
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
-    '''
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
